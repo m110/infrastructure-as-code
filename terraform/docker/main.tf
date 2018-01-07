b64edace48b5fa1fe8f05d215239cd2773d06421
Miłosz Smółka <m110@m110.pl>
2018-01-07 14:08:29 +0100

    Experimental load balancer backends updating by null resource / ansible

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -5,7 +5,7 @@ provider "docker" {
 resource "docker_container" "dummy" {
   image = "${docker_image.dummy_server.latest}"
   name  = "dummy-server-${count.index}"
-  count = 4
+  count = 2
 }
 
 resource "docker_container" "lbc" {
@@ -17,11 +17,6 @@ resource "docker_container" "lbc" {
     internal = 80
     external = 8080
   }
-
-  upload {
-    file    = "/etc/nginx/conf.d/backends"
-    content = "${join("\n", formatlist("server %s:${var.dummy_server_port};", docker_container.dummy.*.ip_address))}\n"
-  }
 }
 
 resource "docker_image" "dummy_server" {
@@ -30,4 +25,28 @@ resource "docker_image" "dummy_server" {
 
 resource "docker_image" "lbc" {
   name = "${var.docker_registry_url}/dummy-lbc:0.1"
+}
+
+resource "null_resource" "backends" {
+  triggers {
+    backends_ips = "${join(",", docker_container.dummy.*.ip_address)}"
+  }
+
+  provisioner "local-exec" {
+    command = "ANSIBLE_CONFIG=../../ansible/ansible.cfg ansible-playbook ../../ansible/load-balancer.yml -t nginx-config -D -e '${jsonencode(local.ansible_vars)}' -i ${docker_container.lbc.ip_address},"
+  }
+}
+
+locals {
+  ansible_vars = {
+    nginx = {
+      become   = false
+      backends = "${formatlist("%s:${var.dummy_server_port}", docker_container.dummy.*.ip_address)}"
+    }
+
+    ansible_connection        = "docker"
+    ansible_docker_extra_args = "-H=${var.docker_host}"
+    ansible_host              = "${docker_container.lbc.name}"
+    host                      = "${docker_container.lbc.ip_address}"
+  }
 }
