e822b1df156058ea457611ed0a14b88f8c207486
Miłosz Smółka <m110@m110.pl>
2018-02-03 13:55:05 +0100

    Add hcloud packer files

---
+++ packer/hcloud/README.md
@@ -0,0 +1,1 @@
+Requires hcloud packer plugin: https://github.com/m110/packer-builder-hcloud
--- packer/scripts/python.sh
+++ packer/scripts/python.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-apt-get update && apt-get install -y --no-install-recommends python
+while [ 1 ]; do
+    apt-get update
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    sleep 2
+done
+apt-get install -y --no-install-recommends python
